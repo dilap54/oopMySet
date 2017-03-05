8af7cc8c709c0dc5e0c3e394eebf2c45ac6c9867
dilap54 <dilap54@DESKTOP-4EQ7QVU>
2017-03-05 21:14:13 +0300

    Добавьте файлы проекта.

---
+++ MySet/Class1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySet
+{
+    //Кароче это класс исключений
+    [Serializable]
+    class SetException : Exception
+    {
+        public SetException(string message) : base(message)
+        {
+
+        }
+    }
+
+    //Базовый класс
+    public abstract class ISet<T>: IEnumerable
+    {
+        //Объявление метода, нужного для IEnumerable
+        public abstract IEnumerator GetEnumerator();
+
+        /*
+        //Тоже нужно для IEnumerable. И но я хз что это.
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        */
+
+        //Кучка нужных методов, реализуемых в дочерних классах
+        public abstract void Add(T value);
+
+        public abstract void Clear();
+
+        public abstract bool Contains(T value);
+
+        public abstract void Remove(T value);
+
+        public abstract int Count
+        {
+            get;
+        }
+
+        public abstract bool isEmpty
+        {
+            get;
+        }
+    }
+
+    public class ArraySet<T> : ISet<T> where T : IEquatable<T>
+    {
+        T[] array;
+
+        public override int Count
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public override bool isEmpty
+        {
+            get
+            {
+                return Count == 0;
+            }
+        }
+
+        public ArraySet(){
+            array = new T[0];
+        }
+
+        public override void Add(T value)
+        {
+            Array.Resize<T>(ref array, Count+1);
+            array[Count - 1] = value;
+        }
+
+        public override void Clear()
+        {
+            array = new T[0];
+        }
+
+        public override bool Contains(T value)
+        {
+            foreach (T item in array)
+            {
+                //Если ссылки равны
+                if (item.Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return array.GetEnumerator();
+        }
+
+        public override void Remove(T value)
+        {
+            array = array.Where((item) => {
+                return !(item.Equals(value));
+            }).ToArray();
+        }
+    }
+    
+}
---
+++ MySetTests/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySet;
+namespace MySetTests
+{
+    [TestClass]
+    public class ArraySetTest
+    {
+        [TestMethod]
+        public void CreateArraySet()
+        {
+            ArraySet<int> arraySet = new ArraySet<int>();
+        }
+          
+        [TestMethod]
+        public void AddItemMustIncreaseCount()
+        {
+            ArraySet<int> arraySet = new ArraySet<int>();
+            int count = arraySet.Count;
+            arraySet.Add(1);
+            Assert.IsTrue(arraySet.Count > count);
+
+            count = arraySet.Count;
+            arraySet.Add(1);
+            Assert.IsTrue(arraySet.Count > count);
+        }
+
+        [TestMethod]
+        public void RemoveExistedItemDecreaseCount()
+        {
+            ArraySet<int> arraySet = new ArraySet<int>();
+            arraySet.Add(1);
+            int count = arraySet.Count;
+            arraySet.Remove(1);
+            Assert.IsTrue(arraySet.Count < count);
+        }
+
+        [TestMethod]
+        public void RemoveNonExistedItemDecreaseCount()
+        {
+            ArraySet<int> arraySet = new ArraySet<int>();
+            arraySet.Add(1);
+            int count = arraySet.Count;
+            arraySet.Remove(10);
+            Assert.AreEqual(count, arraySet.Count);
+        }
+
+        [TestMethod]
+        public void ClearClearsSet()
+        {
+            ArraySet<int> arraySet = new ArraySet<int>();
+            arraySet.Add(1);
+            arraySet.Add(2);
+            arraySet.Add(3);
+            arraySet.Clear();
+
+            Assert.AreEqual(0, arraySet.Count);
+        }
+    }
+}
